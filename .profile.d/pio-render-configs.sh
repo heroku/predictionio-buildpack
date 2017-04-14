d635816a4c219e6d59e27071e3dc69ab15c7264d
Mars Hall <mars@heroku.com>
2017-04-14 15:22:46 -0700

    Render configs from current working directory.

--- .profile.d/pio-render-configs.sh
+++ .profile.d/pio-render-configs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-core_site_template=/app/pio-engine/PredictionIO-dist/conf/core-site.xml.erb
+core_site_template=pio-engine/PredictionIO-dist/conf/core-site.xml.erb
 
 if [ -f "$core_site_template" ]
 then
-  erb $core_site_template > /app/pio-engine/PredictionIO-dist/conf/core-site.xml
+  erb $core_site_template > pio-engine/PredictionIO-dist/conf/core-site.xml
 fi
