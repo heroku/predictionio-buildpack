40f4a6822e52979d17f7ea5f6e8195c47f4a3380
Mars Hall <mars@heroku.com>
2017-04-25 14:19:51 -0700

    Implement Spark defaults config template w/ Elasticsearch settings

--- .profile.d/pio-render-configs.sh
+++ .profile.d/pio-render-configs.sh
@@ -2,6 +2,7 @@
 
 core_site_template=pio-engine/PredictionIO-dist/conf/core-site.xml.erb
 elasticsearch_template=pio-engine/PredictionIO-dist/conf/elasticsearch.yml.erb
+spark_template=pio-engine/PredictionIO-dist/vendors/spark-hadoop/conf/spark-defaults.conf.erb
 
 if [ -f "$core_site_template" ]
 then
@@ -11,4 +12,9 @@ fi
 if [ -f "$elasticsearch_template" ]
 then
   erb $elasticsearch_template > pio-engine/PredictionIO-dist/conf/elasticsearch.yml
+fi
+
+if [ -f "$spark_template" ]
+then
+  erb $spark_template > pio-engine/PredictionIO-dist/vendors/spark-hadoop/conf/spark-defaults.conf
 fi
